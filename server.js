3e5b15aff3c98fc21d152ce940d174169f8a2ac4
Julia Edwards <jedwardsjunior@gmail.com>
2015-07-21 17:25:44 -0400

    Removed Jade, incorporating Angular

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
+var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var request        = require("request");
 // configuration ===========================================
 var port = process.env.PORT || 8080; // set our port
+var db = require('./config/db');
+mongoose.connect(db.url); // connect to our mongoDB database
 
 
 // setup middleware
@@ -16,19 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-f
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
-
-// Set our default template engine to "jade"
-// which prevents the need for extensions
-// (although you can still mix and match)
-app.set('views', __dirname + '/public/views');
-app.set('view engine', 'jade');
-
-
-app.get('/', function(req, res){
-  res.render('index');
-});
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
-app.listen(port);
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-exports = module.exports = app; 						// expose app
+app.listen(port);// startup our app at http://localhost:8080
+console.log('Magic happens on port ' + port); // shoutout to the user
+exports = module.exports = app; // expose app
